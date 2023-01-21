ce3841310b6a8562f02321ec9a4143c6e6eb8043
MaganaAsiati <assia.magana@gmail.com>
2023-01-21 19:21:28 +0200

    Add views

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,4 +1,8 @@
 class Deal < ApplicationRecord
-  belongs_to :user
-  belongs_to :category
+  belongs_to :user, class_name: 'User', foreign_key: :user_id
+  belongs_to :category, class_name: 'Category', foreign_key: :category_id
+
+  validates :name, length: { minimum: 1 }
+  validates :amount, numericality: { only_integer: true, greater_than: 0 }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         has_many :categories, dependent: :destroy
+         has_many :deals, dependent: :destroy
+
+  validates :name, length: { minimum: 1 }
 end
