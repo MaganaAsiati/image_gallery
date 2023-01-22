a1c7bd4dc53d6e4e8c7410c6bc1498b3d5baaa16
MaganaAsiati <assia.magana@gmail.com>
2023-01-22 17:08:05 +0200

    Changes message after create the picture

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -33,7 +33,7 @@ class CategoriesController < ApplicationController
     respond_to do |format|
       if @category.save
         format.html do
-          redirect_to categories_path, notice: 'Category was successfully created.'
+          redirect_to categories_path, notice: 'Picture was successfully created.'
         end
         format.json { render :show, status: :created, location: @category }
       else
@@ -47,7 +47,7 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to category_url(@category), notice: 'Category was successfully updated.' }
+        format.html { redirect_to category_url(@category), notice: 'Picture was successfully updated.' }
         format.json { render :show, status: :ok, location: @category }
       else
         format.html { render :edit, status: :unprocessable_entity }
